6aacd916ec87a2c050d8c3b6209a102dbd25c837
Divyansh Kushwaha <divyanshkushwaha@Divyanshs-MacBook-Air.local>
2021-11-22 13:03:47 +0530

    second commit

--- app/src/main/java/com/example/memorygame/SplashScreen.kt
+++ app/src/main/java/com/example/memorygame/SplashScreen.kt
@@ -5,8 +5,11 @@ import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class SplashScreen : AppCompatActivity() {
@@ -24,7 +27,11 @@ class SplashScreen : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+        val txtView=findViewById<TextView>(R.id.txtView)
+        val anim=AnimationUtils.loadAnimation(this,R.anim.fade_out)
+        txtView.startAnimation(anim)
         Handler().postDelayed({
+            txtView.visibility=View.GONE
             val i= Intent(this,MainActivity::class.java)
             startActivity(i)
             finish()
